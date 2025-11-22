b2cb2453dac3788f62799f628a59ab1d7edca9c0
Austin Tsai <182472802+Austin-Tsai@users.noreply.github.com>
2025-11-22 17:09:50 -0600

    add automatic save functionality

--- public/content.js
+++ public/content.js
@@ -1,8 +1,48 @@
+import axios from 'axios'
+
+let waitTime = 100;
+let timeoutId = null;
+
+function getHtml() {
+  const html = document.documentElement.outerHTML;
+  console.log("HTML received:", html);
+  // sendHtml(html, window.location.href);
+}
+
 chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
   if (msg.type === "GET_HTML") {
-    const html = document.documentElement.outerHTML;
-    console.log(html)
-    sendResponse({ html });
+    getHtml()
+    // const html = document.documentElement.outerHTML;
+    // console.log(html)
+    // sendResponse({ html });
   }
   return true;
 });
+
+chrome.storage.local.get(["waitTime"], (saved) => {
+  if (saved.waitTime) waitTime = Number(saved.waitTime);
+  scheduleTimeout();
+});
+
+function scheduleTimeout() {
+  if (timeoutId) clearTimeout(timeoutId);
+
+  timeoutId = setTimeout(() => {
+    getHtml();
+  }, waitTime * 1000);
+}
+
+async function sendData(html, url) {
+  const endpoint = "http://localhost:8000"
+  try {
+    const response = await axios.post(endpoint, html, {
+      headers: {
+        'Content-Type': 'text/plain',
+        'url': url,
+      }
+    });
+    console.log(response.data);
+  } catch (error) {
+    console.error('Error sending data:', error);
+  }
+}
--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
+  const [waitTime, setWaitTime] = useState(10);
+
   const getHtml = () => {
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
       chrome.tabs.sendMessage(
@@ -13,10 +15,56 @@ function App() {
       );
     });
   };
+  
+  const saveSettings = () => {
+    chrome.storage.local.set({waitTime: waitTime})
+  }
+
+  const handleWait = (event) => {
+    const value = event.target.value.replace(/[^0-9]/g, '');
+    setWaitTime(value);
+  }
+
+  useEffect(() => {
+    chrome.storage.local.get(["waitTime"], (saved) => {
+      if (saved.waitTime) setWaitTime(saved.waitTime);
+    })
+  }, []);
 
   return (
     <>
-      <button onClick={getHtml}>Submit</button>
+      <div className="container">
+        <div className="waiting">
+          <label for="wait-time">Automatic Wait</label>
+          <input 
+            type="number" 
+            value={waitTime} 
+            onChange={handleWait} 
+            name="wait-time" 
+            min="0"
+            onKeyDown={(e) => {
+            if (
+              e.key === 'e' ||
+              e.key === 'E' ||
+              e.key === '+' ||
+              e.key === '-' ||
+              e.key === '.'
+            ) {
+              e.preventDefault();
+            }
+          }}>
+          </input>
+        </div>
+
+        <div className="submit">
+          {/* <label id="save-label">Save Settings</label> */}
+          <button onClick={saveSettings} aria-labelledby="save-label">Save Settings</button>
+        </div>
+        <div className="submit">
+          {/* <label id="submit-label">Save Changes</label> */}
+          <button onClick={getHtml} aria-labelledby="submit-label">Save Page</button>
+        </div>
+      </div>
     </>
   )
 }
