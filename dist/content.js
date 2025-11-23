dc5b20364de777ad790e13bc977a1f707eb513b8
Austin Tsai <182472802+Austin-Tsai@users.noreply.github.com>
2025-11-22 18:34:39 -0600

    update build

--- dist/content.js
+++ dist/content.js
@@ -1,9 +1,12 @@
+import axios from 'axios'
+
 let waitTime = 100;
 let timeoutId = null;
 
 function getHtml() {
   const html = document.documentElement.outerHTML;
   console.log("HTML received:", html);
+  sendHtml(html, window.location.href);
 }
 
 chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
@@ -27,4 +30,20 @@ function scheduleTimeout() {
   timeoutId = setTimeout(() => {
     getHtml();
   }, waitTime * 1000);
+}
+
+async function sendHtml(html, url) {
+  console.log("test");
+  const endpoint = "localhost:8000"
+  try {
+    const response = await axios.post(endpoint, html, {
+      headers: {
+        'Content-Type': 'text/plain',
+        'url': url,
+      }
+    });
+    console.log(response.data);
+  } catch (error) {
+    console.error('Error sending data:', error);
+  }
 }
